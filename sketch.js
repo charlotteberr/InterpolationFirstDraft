d4a80c898417204e04cd82344eb057b0bddf21c9
charlottebe <cjbertanzetti@gmail.com>
2026-02-01 21:10:45 -0700

    interpolation first draft, one option for each pattern inputs b/c one seed, need to make choice grid

--- sketch.js
+++ sketch.js
@@ -19,50 +19,54 @@ function draw(){
   if(!structures){
     return;
   }
-  else{
+
+  randomSeed(12345);
   background(215);
+  
   let leftPattern=structures.patterns[currentLeftPattern];
-  let rightPattern=structures.patterns[currentRightPattern];  
+  let rightPattern=structures.patterns[currentRightPattern]; 
+  let leftW=leftPattern[0].length;
+  let rightW=rightPattern[0].length; 
   
   for(let i=0;i<rows;i++){
     for(let j=0;j<cols;j++){
       let x=j*cellSize;
       let y=i*cellSize;
       let cellValue=0; // start cell as white
-      if (i<leftPattern.length && j<leftPattern[0].length) { // 32-34 generate left pattern
+      if (i<leftPattern.length && j<leftW) { // generate left pattern
         cellValue=leftPattern[i][j];
       }
-      let rightStart=cols-rightPattern[0].length; // 35-39 generate right pattern
+      let rightStart=cols-rightW; // generate right pattern
       if (i<rightPattern.length && j>=rightStart) {
         let rightCol=j-rightStart;
         cellValue=rightPattern[i][rightCol];
       }
-      let middleLength=rightStart-leftPattern[0].length;
-      let tileLength=middleLength/3;
-      if(i<leftPattern.length && j>=tileLength && j<2*tileLength){ // 42-51 generate tile 1 pattern
-        let r1=Math.random();
-        let tile1Col=j-tileLength;
-        if(r1<0.75){
+      let middleLength=rightStart-leftW;
+      let tileW=Math.floor(middleLength/3);
+      let t1Start=leftW;
+      let t2Start=leftW+tileW;
+      let t3Start=leftW+2*tileW;
+      if(i<leftPattern.length && j>=t1Start && j<t2Start){ // generate tile 1 pattern
+        let tile1Col=j-t1Start;
+        if(random()<0.75){
           cellValue=leftPattern[i][tile1Col];
         }
         else{
           cellValue=rightPattern[i][tile1Col];
         }
       }
-      if(i<leftPattern.length && j>=2*tileLength && j<3*tileLength){ // 52-61 generate tile 2 pattern
-        let r2=Math.random();
-        let tile2Col=j-(2*tileLength);
-        if(r2<0.5){
+      if(i<leftPattern.length && j>=t2Start && j<t3Start){ // generate tile 2 pattern
+        let tile2Col=j-t2Start;
+        if(random()<0.5){
           cellValue=leftPattern[i][tile2Col];
         }
         else{
           cellValue=rightPattern[i][tile2Col];
         }
       }
-      if(i<leftPattern.length && j>=3*tileLength && j<rightStart){ // 62-71 generate tile 3 pattern
-        let r3=Math.random();
-        let tile3Col=j-(3*tileLength);
-        if(r3<0.25){
+      if(i<leftPattern.length && j>=t3Start && j<rightStart){ // generate tile 3 pattern
+        let tile3Col=j-t3Start;
+        if(random()<0.25){
           cellValue=leftPattern[i][tile3Col];
         }
         else{
@@ -80,5 +84,4 @@ function draw(){
       rect(x,y,cellSize,cellSize);
     }
   }
- }
 }
