ba2b4b2a925c149b9839f403569fe900d5ac30f8
charlottebe <cjbertanzetti@gmail.com>
2026-02-17 13:08:55 -0700

    Fixed interpolation zone issue and added a way to smooth black and white cells for interpolation

--- sketch.js
+++ sketch.js
@@ -7,10 +7,14 @@ let currentLeftPattern="random";
 let currentRightPattern="random"; 
 
 let choiceGrid=[];
+let copyGrid=[];
 
-let leftInput, rightInput, middleInput, interpInput, helpText;
+let leftInput, rightInput, middleInput, interpInput, whiteSmoothCheckbox, blackSmoothCheckbox, helpText;
 let middleLengthValue=18; // default middle area width in columns
 let interpPercent=50;
+let smoothInterp=false;
+let whiteSmooth=false;
+let blackSmooth=false;
 
 function setup(){
   createCanvas(cols*cellSize,rows*cellSize);
@@ -35,6 +39,14 @@ function setup(){
   interpInput.position(600, height + 40);
   interpInput.input(updateInterpolation);
 
+  createP("Smooth Interpolation").position(820, height + 5);
+  whiteSmoothCheckbox = createCheckbox("White", whiteSmooth);
+  whiteSmoothCheckbox.position(820, height + 35);
+  whiteSmoothCheckbox.changed(updateSmoothGradient);
+  blackSmoothCheckbox = createCheckbox("Black", blackSmooth);
+  blackSmoothCheckbox.position(900, height + 35);
+  blackSmoothCheckbox.changed(updateSmoothGradient);
+
   helpText = createP("Possible patterns: (loading...)"); // list of pattern names
   helpText.position(10, height + 70);
 
@@ -50,6 +62,82 @@ function buildChoiceGrid(){     // creates a 2D array the same size as canvas
       choiceGrid[i][j]=Math.random();
     }
   }
+}
+
+function buildCopyGrid(){  // copy of filled canvas of 0 and 1
+  copyGrid=[];
+  for(let i=0;i<rows;i++){
+    copyGrid[i]=[];
+    for(let j=0;j<cols;j++){
+      copyGrid[i][j]=0;
+    }
+  }
+}
+
+function editCopyGrid(leftW, rightStart){
+  let newGrid=[];
+  for(let i=0;i<rows;i++){
+    newGrid[i]=[];
+    for(let j=0;j<cols;j++){
+      newGrid[i][j]=copyGrid[i][j];
+    }
+  }
+  for(let i=0;i<rows;i++){
+    for(let j=0;j<cols;j++){
+      let topLeft=0;
+      let topMiddle=0;
+      let topRight=0;
+      let middleLeft=0;
+      let middleRight=0;
+      let bottomLeft=0; 
+      let bottomMiddle=0;
+      let bottomRight=0;
+      let totalCells=0;
+      if(i-1>=0 && j-1>=0){
+        topLeft=copyGrid[i-1][j-1];
+        totalCells++;
+      }
+      if(i-1>=0){
+        topMiddle=copyGrid[i-1][j];
+        totalCells++;
+      }
+      if(i-1>=0 && j+1<cols){
+        topRight=copyGrid[i-1][j+1];
+        totalCells++;
+      }
+      if(j-1>=0){
+        middleLeft=copyGrid[i][j-1];
+        totalCells++;
+      }
+      if(j+1<cols){
+        middleRight=copyGrid[i][j+1];
+        totalCells++;
+      }
+      if(i+1<rows && j-1>=0){
+        bottomLeft=copyGrid[i+1][j-1];
+        totalCells++;
+      }
+      if(i+1<rows){
+        bottomMiddle=copyGrid[i+1][j];
+        totalCells++;
+      }
+      if(i+1<rows && j+1<cols){
+        bottomRight=copyGrid[i+1][j+1];
+        totalCells++;
+      }
+      let blackCount=topLeft+topMiddle+topRight+middleLeft+middleRight+bottomLeft+bottomMiddle+bottomRight;
+      let whiteCount=totalCells-blackCount;
+      if(totalCells>=6 && j>=leftW && j<rightStart){
+        if(copyGrid[i][j]===0 && blackCount>=6 && blackSmooth){
+          newGrid[i][j]=1;
+        }
+        if(copyGrid[i][j]===1 && whiteCount>=6 && whiteSmooth){  
+          newGrid[i][j]=0;
+        }
+      }
+    }
+  }
+  copyGrid=newGrid;
 }
 
 function preload(){
@@ -78,6 +166,12 @@ function updateInterpolation(){
   else{
     interpPercent=v;
   }
+}
+
+function updateSmoothGradient(){
+  whiteSmooth = whiteSmoothCheckbox.checked();
+  blackSmooth = blackSmoothCheckbox.checked();
+  smoothInterp = whiteSmooth || blackSmooth;
 }
 
 function updateMiddleLength(){
@@ -110,12 +204,12 @@ function draw(){
   let rightPattern=structures.patterns[currentRightPattern]; 
   let leftW=leftPattern[0].length;
   let rightW=rightPattern[0].length;
-  let rightStart=cols-rightW; 
+  let rightStart=cols-rightW;
+  
+  buildCopyGrid();
   
   for(let i=0;i<rows;i++){
     for(let j=0;j<cols;j++){
-      let x=j*cellSize;
-      let y=i*cellSize;
       let cellValue=0; // start cell as white
       let r=choiceGrid[i][j];
 
@@ -132,27 +226,39 @@ function draw(){
 
       if (i<leftPattern.length && j<leftW){  // left pattern
         cellValue=leftPattern[i][j];
+        copyGrid[i][j]=cellValue;
       } 
       else if (i<rightPattern.length && j>=rightStart){ // right pattern
         let rightCol=j-rightStart;
         cellValue=rightPattern[i][rightCol];
+        copyGrid[i][j]=cellValue;
       }
       else if (j>=leftW && j<rightStart){ 
         if(i<leftPattern.length && j<remainderStart && leftTileCount>0){  // left tiles before remainder
           let leftTileIndex=Math.floor((j-leftW)/tileW);  // gives which left tile your in, ex. tiles are 6 columns wide, if youre 8 columns in youre in 2nd left tile, index 1
           let leftProb=0.9;
-          if(interpPercent>=65 && leftTileCount>=2){  // Left tiles broken up into 70/30 to make probability grow at different rates to show zones better
+          if(interpPercent>=60 && leftTileCount>=2){  // Left tiles broken up into 70/30 to make probability grow at different rates to show zones better
             let left1Count=Math.round(leftTileCount*0.7);
             let left2Count=leftTileCount-left1Count;
             if(leftTileIndex<left1Count){
-              leftProb=0.9-((leftTileIndex/(left1Count-1))*0.2); // 0.9 to 0.7
-            // leftTileIndex-(left1Count-1) gives number from 0-1
-            // multiplied by 0.2 then subtracted from 0.9 gives probability from 90% to 70% of left pattern before remainder
-            // every tile has its own probability, but each cell in that tile is evaluated seperatly running that probabilty again
+              if(left1Count>1){
+                leftProb=0.9-((leftTileIndex/(left1Count-1))*0.2); // 0.9 to 0.7
+                // leftTileIndex-(left1Count-1) gives number from 0-1
+                // multiplied by 0.2 then subtracted from 0.9 gives probability from 90% to 70% of left pattern before remainder
+                // every tile has its own probability, but each cell in that tile is evaluated seperatly running that probabilty again
+              }
+              else{
+                leftProb=0.8;
+              }
             }
             else{
               let secondIndex=leftTileIndex-left1Count;
-              leftProb=0.7-((secondIndex/(left2Count-1))*0.2); // 0.7 to 0.5
+              if(left2Count>1){
+                leftProb=0.7-((secondIndex/(left2Count-1))*0.2); // 0.7 to 0.5
+              }
+              else{
+                leftProb=0.6;
+              }
             }
           }
           else{
@@ -166,33 +272,47 @@ function draw(){
           let tileCol=j-(leftW+(leftTileIndex*tileW)); // map to pattern column
           if(r<leftProb){
             cellValue=leftPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
           else{
             cellValue=rightPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
         }
         else if(remainder>0 && i<leftPattern.length && j>=remainderStart && j<remainderEnd){ // remainder, always 50/50 chance
           let tileCol=j-remainderStart;
           if(r<0.5){
             cellValue=leftPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
           else{
             cellValue=rightPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
         }
         else if(i<leftPattern.length && j>=remainderEnd && rightTileCount>0){ // right tiles, works the same as left but probability goes 50% to 10% chance of left pattern
           let rightSideStart=remainderEnd;
           let rightTileIndex=Math.floor((j-rightSideStart)/tileW);
           let rightProb=0.1;
-          if(interpPercent<=35 && rightTileCount>=2){
+          if(interpPercent<=40 && rightTileCount>=2){
             let right2Count=Math.round(rightTileCount*0.7);
             let right1Count=rightTileCount-right2Count;
             if(rightTileIndex<right1Count){
-              rightProb=0.5-((rightTileIndex/(right1Count-1))*0.2); // 0.5 to 0.3
+              if(right1Count>1){
+                rightProb=0.5-((rightTileIndex/(right1Count-1))*0.2); // 0.5 to 0.3
+              }
+              else{
+                rightProb=0.4;
+              }
             }
             else{
               let secondIndex=rightTileIndex-right1Count;
-              rightProb=0.3-((secondIndex/(right2Count-1))*0.2); // 0.3 to 0.1
+              if(right2Count>1){
+                rightProb=0.3-((secondIndex/(right2Count-1))*0.2); // 0.3 to 0.1
+              }
+              else{
+                rightProb=0.2;
+              }
             }
           }
           else{
@@ -206,21 +326,49 @@ function draw(){
           let tileCol=j-(rightSideStart+(rightTileIndex*tileW));
           if(r<rightProb){
             cellValue=leftPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
           else{
             cellValue=rightPattern[i][tileCol];
+            copyGrid[i][j]=cellValue;
           }
         }
       }
-      if (cellValue===1){
+    }
+  }
+  if(smoothInterp){
+    editCopyGrid(leftW, rightStart);
+    for(let i=0;i<rows;i++){
+      for(let j=0;j<cols;j++){
+        let x=j*cellSize;
+        let y=i*cellSize;
+        if (copyGrid[i][j]===1){
         fill(0);
         stroke(255);
-      }
-      else {
+        }
+        else {
         fill(255);
         stroke(0);
+        }
+        rect(x,y,cellSize,cellSize);
       }
-      rect(x,y,cellSize,cellSize);
+    }
+  }
+  else{
+    for(let i=0;i<rows;i++){
+      for(let j=0;j<cols;j++){
+        let x=j*cellSize;
+        let y=i*cellSize;
+        if (copyGrid[i][j]===1){
+        fill(0);
+        stroke(255);
+        }
+        else {
+        fill(255);
+        stroke(0);
+        }
+        rect(x,y,cellSize,cellSize);
+      }
     }
   }
 }
